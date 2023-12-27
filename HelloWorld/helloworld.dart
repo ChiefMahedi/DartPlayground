3d2771fb247e75cbfcc4b157bf4ce4110b9c57f0
Shawon <chiefmahedi@gmail.com>
2023-12-27 22:49:02 +0600

    Added descriptive comments to the helloworld.dart program

--- HelloWorld/helloworld.dart
+++ HelloWorld/helloworld.dart
@@ -2,5 +2,5 @@
 //In this code main function's return type is void
 void main() {
   //To display text on console top level print function can be used
-  print("Hello World!");
+  print("Hello World!"); //Output: Hello world
 }
