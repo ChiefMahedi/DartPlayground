a39cc738828ce17cb9a7281c80f2c728b07b364a
Shawon <chiefmahedi@gmail.com>
2023-12-27 22:46:35 +0600

    Added comment to describe the helloworld.dart program

--- HelloWorld/helloworld.dart
+++ HelloWorld/helloworld.dart
@@ -1,3 +1,6 @@
+//Top level main function where program execution starts.
+//In this code main function's return type is void
 void main() {
+  //To display text on console top level print function can be used
   print("Hello World!");
 }
