9a7a027794e04916d47d6ca44be75530fb8fb2a8
Shawon <chiefmahedi@gmail.com>
2023-12-31 12:31:17 +0600

    Add Numbers data type related code

---
+++ Basic/Types/Numbers/numbers.dart
@@ -0,0 +1,23 @@
+//Author: Mahedi Hassan Shawon
+//Notes can be found on numbers.md file
+void main() {
+  //Numbers
+  var ten = 10;
+  print(ten);
+  num number = 20;
+  print(number);
+  var onePointFive = 1.52345;
+  print(onePointFive);
+  var exponents = 1.5e5;
+  print(exponents);
+  double x = 2;
+  print(x);
+  var two = int.parse('2');
+  print(two);
+  var twoPointFile = double.parse('2.5');
+  print(twoPointFile);
+  String tenAsString = ten.toString();
+  print(tenAsString);
+  String onePointFiveAsString = onePointFive.toStringAsFixed(2);
+  print(onePointFiveAsString);
+}
